8b136c381c37f6498ecfb9570148ec176ca12afb
Lukas Zeller <luz@plan44.ch>
2014-01-18 18:58:22 +0100

    Updated to Pebble SDK 2.0

--- README.md
+++ README.md
@@ -12,6 +12,6 @@ and the license text* when you distribute apps based on this code.
 
 **Important**: This code is based on Pebble's early beta or even alpha proof-of-concept SDK, so it is highly probable this code will break and need adaption in the near future as Pebble SDK matures. **Use entirely on you own risk**.
 
-To install and use the pebble SDK, please follow the instructions at [developer.getpebble.com](http://developer.getpebble.com/1/welcome).
+To install and use the pebble SDK, please follow the instructions at [developer.getpebble.com/2](https://developer.getpebble.com/2/).
 
-To download the already compiled watchface (.pbw file) just see the built_app/ folder.
+To install the compiled watchface, just go to the [pebble appstore] (https://getpebble.com), or use the already compiled watchface (.pbw file) in the built_app/ folder.
--- src/timedot.c
+++ src/timedot.c
@@ -1,30 +1,20 @@
 /*
 
   TimeDot watch
-  (c) 2013 by Lukas Zeller, plan44.ch
+  (c) 2013-2014 by Lukas Zeller, plan44.ch
 
  */
 
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
-
-
-#define MY_UUID { 0x9B, 0x3A, 0xAB, 0x35, 0xB0, 0x92, 0x48, 0xE9, 0x80, 0x02, 0xDC, 0x8F, 0x57, 0x2C, 0x50, 0xBC }
-PBL_APP_INFO(MY_UUID,
-             "TimeDot", "plan44.ch",
-             1, 0, /* App version */
-             RESOURCE_ID_IMAGE_MENU_ICON,
-             APP_INFO_WATCH_FACE);
+#include <pebble.h>
 
 // Variables
 // - the window
-Window window;
+Window *window;
 // - layers
-Layer second_display_layer;
-TextLayer time_display_textlayer;
-TextLayer weekday_display_textlayer;
-TextLayer date_display_textlayer;
+Layer *second_display_layer;
+TextLayer *time_display_textlayer;
+TextLayer *weekday_display_textlayer;
+TextLayer *date_display_textlayer;
 
 // - internal variables
 int currentSecond;
@@ -39,7 +29,7 @@ char date_str_buffer[TIME_STR_BUFFER_BYTES];
 // Geometry constants
 #define TIMETEXT_LAYER_MINMARGIN 13
 #define TIMETEXT_LAYER_EXTRAMARGIN_H 36
-#define TIMETEXT_FONT FONT_KEY_GOTHAM_42_MEDIUM_NUMBERS
+#define TIMETEXT_FONT FONT_KEY_BITHAM_42_MEDIUM_NUMBERS
 
 #define AUXTEXT_LAYER_EXTRAMARGIN_H 17
 #define AUXTEXT_H 20
@@ -49,11 +39,12 @@ char date_str_buffer[TIME_STR_BUFFER_BYTES];
 #define DOT_RADIUS 4
 
 
-void second_display_layer_callback(Layer *me, GContext* ctx)
+void second_display_layer_callback(struct Layer *layer, GContext *ctx)
 {
   // unsigned int angle = currentSecond * 6;
   int32_t hexAngle = (int32_t)currentSecond * 1092;
-  GPoint center = grect_center_point(&me->frame);
+  GRect f = layer_get_frame(layer);
+  GPoint center = grect_center_point(&f);
   int32_t r = center.x-DOT_MARGIN;
   GPoint dotPos;
   dotPos.x = center.x + sin_lookup(hexAngle)*r/TRIG_MAX_RATIO;
@@ -68,114 +59,124 @@ void second_display_layer_callback(Layer *me, GContext* ctx)
 //#define DEBUG_TIME_STRING "23:33" // fattest time
 
 // handle second tick
-void handle_second_tick(AppContextRef ctx, PebbleTickEvent *event)
+void handle_second_tick(struct tm *tick_time, TimeUnits units_changed)
 {
-  currentSecond = event->tick_time->tm_sec;
-  if (currentSecond==0) timeTextValid = 0; // new minute, needs new text
+  currentSecond = tick_time->tm_sec;
+  if (units_changed & MINUTE_UNIT) timeTextValid = 0; // new minute, needs new text
   if (!timeTextValid) {
     // update time text
-    if (event->tick_time->tm_min==0 && event->tick_time->tm_hour==0) dateTextValid = 0;
+    if (units_changed & DAY_UNIT) dateTextValid = 0;
     #ifdef DEBUG_TIME_STRING
-    text_layer_set_text(&time_display_textlayer, DEBUG_TIME_STRING);
+    text_layer_set_text(time_display_textlayer, DEBUG_TIME_STRING);
     #else
     if (clock_is_24h_style())
-      string_format_time(time_str_buffer, TIME_STR_BUFFER_BYTES, "%H:%M", event->tick_time);
+      strftime(time_str_buffer, TIME_STR_BUFFER_BYTES, "%H:%M", tick_time);
     else
-      string_format_time(time_str_buffer, TIME_STR_BUFFER_BYTES, "%I:%M", event->tick_time);
-    text_layer_set_text(&time_display_textlayer, time_str_buffer);
+      strftime(time_str_buffer, TIME_STR_BUFFER_BYTES, "%I:%M", tick_time);
+    text_layer_set_text(time_display_textlayer, time_str_buffer);
     #endif
     timeTextValid = 1; // now valid
   }
   if (!dateTextValid) {
     // update weekday text
-    string_format_time(weekday_str_buffer, TIME_STR_BUFFER_BYTES, "%A", event->tick_time);
-    text_layer_set_text(&weekday_display_textlayer, weekday_str_buffer);
+    strftime(weekday_str_buffer, TIME_STR_BUFFER_BYTES, "%A", tick_time);
+    text_layer_set_text(weekday_display_textlayer, weekday_str_buffer);
     // update date text
-    string_format_time(date_str_buffer, TIME_STR_BUFFER_BYTES, "%Y-%m-%d", event->tick_time);
-    text_layer_set_text(&date_display_textlayer, date_str_buffer);
+    strftime(date_str_buffer, TIME_STR_BUFFER_BYTES, "%Y-%m-%d", tick_time);
+    text_layer_set_text(date_display_textlayer, date_str_buffer);
   }
   // anyway, update second
-  layer_mark_dirty(&second_display_layer);
+  layer_mark_dirty(second_display_layer);
 }
 
 
 
-void handle_init(AppContextRef ctx)
+void init()
 {
-  (void)ctx;
-
   // the window
-  window_init(&window, "TimeDot");
-  window_set_background_color(&window, GColorBlack);
-  window_stack_push(&window, true /* Animated */);
+  window = window_create();
+  window_set_background_color(window, GColorBlack);
+  window_stack_push(window, true /* Animated */);
 
   // init the engine
   timeTextValid = 0; // time text not valid
   dateTextValid = 0; // date text not valid
-  PblTm t;
-  get_time(&t);
-  currentSecond = t.tm_sec; // start with correct second position
+  struct tm *t;
+  time_t now = time(NULL);
+  t = localtime(&now);
+  currentSecond = t->tm_sec; // start with correct second position
 
+  // get the window frame
+  GRect wf = layer_get_frame(window_get_root_layer(window));
   // the text layer for displaying the Ziit time text in big font
-  GRect tf = window.layer.frame;
+  GRect tf = wf;  // start calculation with window frame
   // - vertically centered rectangle for time
   tf.origin.y += (tf.size.h-(tf.size.w-2*TIMETEXT_LAYER_MINMARGIN)) / 2 + TIMETEXT_LAYER_EXTRAMARGIN_H;
   tf.size.h = tf.size.w - TIMETEXT_LAYER_EXTRAMARGIN_H;
-  text_layer_init(&time_display_textlayer, tf);
+  time_display_textlayer = text_layer_create(tf);
+
   // - parameters
-  text_layer_set_text_alignment(&time_display_textlayer, GTextAlignmentCenter); // centered
-  text_layer_set_background_color(&time_display_textlayer, GColorBlack); // black background
-  text_layer_set_font(&time_display_textlayer, fonts_get_system_font(TIMETEXT_FONT)); // font
-  text_layer_set_text_color(&time_display_textlayer, GColorWhite); // white text
-  text_layer_set_text(&time_display_textlayer, "");
-  layer_add_child(&window.layer, &time_display_textlayer.layer);
+  text_layer_set_text_alignment(time_display_textlayer, GTextAlignmentCenter); // centered
+  text_layer_set_background_color(time_display_textlayer, GColorBlack); // black background
+  text_layer_set_font(time_display_textlayer, fonts_get_system_font(TIMETEXT_FONT)); // font
+  text_layer_set_text_color(time_display_textlayer, GColorWhite); // white text
+  text_layer_set_text(time_display_textlayer, "");
+  layer_add_child(window_get_root_layer(window), (Layer *)time_display_textlayer);
 
   // the text layer for displaying the weekday on top of the time
-  GRect wf = window.layer.frame;
+  GRect wdf = wf;
   // - on top of the screen
-  wf.origin.y = -7; // to REALLY use the first pixel on the screen
-  wf.size.h = AUXTEXT_H;
-  text_layer_init(&weekday_display_textlayer, wf);
+  wdf.origin.y = -7; // to REALLY use the first pixel on the screen
+  wdf.size.h = AUXTEXT_H;
+  weekday_display_textlayer = text_layer_create(wdf);
   // - parameters
-  text_layer_set_text_alignment(&weekday_display_textlayer, GTextAlignmentCenter); // centered
-  text_layer_set_background_color(&weekday_display_textlayer, GColorBlack); // black background
-  text_layer_set_font(&weekday_display_textlayer, fonts_get_system_font(AUXTEXT_FONT)); // font
-  text_layer_set_text_color(&weekday_display_textlayer, GColorWhite); // white text
-  text_layer_set_text(&weekday_display_textlayer, "TimeDot © 2013 by");
-  layer_add_child(&window.layer, &weekday_display_textlayer.layer);
+  text_layer_set_text_alignment(weekday_display_textlayer, GTextAlignmentCenter); // centered
+  text_layer_set_background_color(weekday_display_textlayer, GColorBlack); // black background
+  text_layer_set_font(weekday_display_textlayer, fonts_get_system_font(AUXTEXT_FONT)); // font
+  text_layer_set_text_color(weekday_display_textlayer, GColorWhite); // white text
+  text_layer_set_text(weekday_display_textlayer, "TimeDot © 2013 by");
+  layer_add_child(window_get_root_layer(window), (Layer *)weekday_display_textlayer);
 
   // the text layer for displaying the date below the time
-  GRect df = window.layer.frame;
+  GRect df = wf;
   // - at bottom of the screen
   df.origin.y = df.size.h-AUXTEXT_H+2; // to REALLY use the last pixel on the screen
   df.size.h = AUXTEXT_H;
-  text_layer_init(&date_display_textlayer, df);
+  date_display_textlayer = text_layer_create(df);
   // - parameters
-  text_layer_set_text_alignment(&date_display_textlayer, GTextAlignmentCenter); // centered
-  text_layer_set_background_color(&date_display_textlayer, GColorBlack); // black background
-  text_layer_set_font(&date_display_textlayer, fonts_get_system_font(AUXTEXT_FONT)); // font
-  text_layer_set_text_color(&date_display_textlayer, GColorWhite); // white text
-  text_layer_set_text(&date_display_textlayer, "plan44.ch / luz");
-  layer_add_child(&window.layer, &date_display_textlayer.layer);
+  text_layer_set_text_alignment(date_display_textlayer, GTextAlignmentCenter); // centered
+  text_layer_set_background_color(date_display_textlayer, GColorBlack); // black background
+  text_layer_set_font(date_display_textlayer, fonts_get_system_font(AUXTEXT_FONT)); // font
+  text_layer_set_text_color(date_display_textlayer, GColorWhite); // white text
+  text_layer_set_text(date_display_textlayer, "plan44.ch / luz");
+  layer_add_child(window_get_root_layer(window), (Layer *)date_display_textlayer);
 
   // the layer for displaying the seconds dot
-  layer_init(&second_display_layer, window.layer.frame);
-  second_display_layer.update_proc = &second_display_layer_callback;
-  layer_add_child(&window.layer, &second_display_layer);
-  layer_mark_dirty(&second_display_layer); // draw immediately
+  second_display_layer = layer_create(wf);
+  layer_set_update_proc(second_display_layer, &second_display_layer_callback);
+  layer_add_child(window_get_root_layer(window), second_display_layer);
+  layer_mark_dirty(second_display_layer); // draw immediately
 
+  // now subscribe to ticks
+  tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
+}
+
+
+void deinit()
+{
+  layer_destroy(second_display_layer);
+  text_layer_destroy(date_display_textlayer);
+  text_layer_destroy(weekday_display_textlayer);
+  text_layer_destroy(time_display_textlayer);
+  window_destroy(window);
 }
 
 
 
-void pbl_main(void *params)
+int main(void)
 {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-    .tick_info = {
-      .tick_handler = &handle_second_tick,
-      .tick_units = SECOND_UNIT
-    }
-  };
-  app_event_loop(params, &handlers);
+  init();
+  app_event_loop();
+  deinit();
+  return 0;
 }
